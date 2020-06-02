f1505ac847ed228f82c96a0bfdc4badc3a93aea2
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-02 07:46:05 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,10 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+configure :development do
+  set :database, 'sqlite3:db/database.db'
+end
+
 def fi_check_migration
   begin
     ActiveRecord::Migration.check_pending!
@@ -18,6 +22,10 @@ ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
-
+require 'rack-flash'
+require '/home/christopher/playlister-sinatra-online-web-sp-000/app/models/concerns/slugifiable.rb'
 require_all 'app'
 require_all 'lib'
+configure :development do
+  set :database, 'sqlite3:db/database.db'
+end
