e21d95e90966082ccf4b801870c66ab267764b91
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-02 07:30:10 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,13 +14,15 @@ class SongsController < ApplicationController
     erb :'/songs/show'
   end
 
-  post '/songs/:slug' do
-    @song = Song.find_by_slug(params[:slug])
-    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
-    @song.genre_ids = params[:genres]
-    @song.save
-    erb :'/songs/show', locals: {message: "Song successfully updated."}
-  end
+  post '/songs' do
+    #binding.pry
+    if !Song.find_by(name: params[:song][:name])
+      song = Song.create(params[:song])
+
+      if !params[:artist][:name].empty?
+        # update song.artist to value as input by user
+        song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+      end
 
 
   post '/songs' do
