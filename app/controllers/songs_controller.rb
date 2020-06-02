ab5b9df4a8eb476e98d7f0185d7687a2324b3656
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-02 07:29:28 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,6 +14,15 @@ class SongsController < ApplicationController
     erb :'/songs/show'
   end
 
+  post '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+    @song.genre_ids = params[:genres]
+    @song.save
+    erb :'/songs/show', locals: {message: "Song successfully updated."}
+  end
+
+
   post '/songs' do
     @song = Song.create(name: params["Name"])
     @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
@@ -27,13 +36,6 @@ class SongsController < ApplicationController
    erb :'/songs/edit'
  end
 
- post '/songs/:slug' do
-   @song = Song.find_by_slug(params[:slug])
-   @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
-   @song.genre_ids = params[:genres]
-   @song.save
-   erb :'/songs/show', locals: {message: "Song successfully updated."}
- end
 
 
 end
