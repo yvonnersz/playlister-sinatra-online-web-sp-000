31128fc4b47ee3022338e1a14b51e775afce9bb3
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-02 07:46:23 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
+
 class ApplicationController < Sinatra::Base
-  register Sinatra::ActiveRecordExtension
-  set :session_secret, "my_application_secret"
-  set :views, Proc.new { File.join(root, "../views/") }
+register Sinatra::ActiveRecordExtension
 
-  get '/' do
+enable :sessions
+set :session_secret, "my_application_secret"
+set :views, Proc.new { File.join(root, "../views/") }
 
-  end
+get '/' do
+  erb :index
+end
 end
